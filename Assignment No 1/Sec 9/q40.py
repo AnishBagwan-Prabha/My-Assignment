66460a3782050cf209ae657e74a3d25ba0ea91e9
AnishBagwan-Prabha <anishbagwan.prabha@gmail.com>
2026-06-15 11:28:40 +0530

    Add files via upload

---
+++ Assignment No 1/Sec 9/q40.py
@@ -0,0 +1,15 @@
+marks = [90, 80, 85, 75, 95]
+
+average = sum(marks) / len(marks)
+
+print("Average =", average)
+print("Highest =", max(marks))
+
+if average >= 90:
+    print("A")
+elif average >= 75:
+    print("B")
+elif average >= 50:
+    print("C")
+else:
+    print("Fail")
